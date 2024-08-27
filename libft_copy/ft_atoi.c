ea2ffbe4c553f414d297320abe257ecf2cab5a07
lalwafi <lyallwafi@gmail.com>
2024-08-27 11:32:39 +0400

    more parsing...

--- libft_copy/ft_atoi.c
+++ libft_copy/ft_atoi.c
@@ -6,18 +6,32 @@
 /*   By: lalwafi <lalwafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 20:33:14 by lalwafi           #+#    #+#             */
-/*   Updated: 2024/02/20 16:07:48 by lalwafi          ###   ########.fr       */
+/*   Updated: 2024/08/27 10:12:45 by lalwafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	handle_overflow(const char *str, int sign)
+{
+	long	n;
+
+	n = 0;
+	if (*str >= '0' && *str <= '9')
+	{
+		while ((*str >= '0') && (*str <= '9'))
+		{
+			n = n * 10 + (*str - '0');
+			str++;
+		}
+	}
+	return (sign * n);
+}
+
 int	ft_atoi(const char *str)
 {
 	int	sign;
-	int	n;
 
-	n = 0;
 	sign = 1;
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
@@ -28,13 +42,13 @@ int	ft_atoi(const char *str)
 	}
 	else if (*str == '+')
 		str++;
-	if ((*str >= '0') && (*str <= '9'))
-	{
-		while ((*str >= '0') && (*str <= '9'))
-		{
-			n = n * 10 + (*str - '0');
-			str++;
-		}
-	}
-	return (sign * n);
+	return (handle_overflow(str, sign));
+}
+
+int main(void)
+{
+	char *a = "217";
+	int	b;
+	b = ft_atoi(a);
+	printf("%d\n", b);
 }
